705d71b5b5587cbaf5e89b109821a339e6fb0c27
Kajal Pramanik <pramanikkajal7@gmail.com>
2023-02-21 16:25:01 +0530

    Update js code with modules files

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import './modules/constructor.js';
-import './modules/links.js';
+import './modules/navLinks.js';
 import './modules/addRemove.js';
-import './modules/load.js';
+import './modules/windowLoad.js';
 import './modules/addRemoveBtn.js';
-import './modules/datime.js';
+import './modules/dateTime.js';
